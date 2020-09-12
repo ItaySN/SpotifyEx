1ca4a61bad3d2130f837215fb9be635dbc309d39
ItaySN <itayfic@gmail.com>
2020-09-12 19:38:30 +0300

    Start create the server methods
    a GET request to /song/123 returns the details of song 123
    
    a GET request to /artist/123 returns the artist 123
    
    a GET request to /album/123 returns the album 123
    
    a GET request to /playlist/123 returns the playlist 123
    
    a POST request to /album add to album
    
    a POST request to /playlist add to playlist
    
    a POST request to /artist add to artist

--- Index.js
+++ Index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const mysql = require('mysql');
 require('dotenv').config();
 
+const app = express();
+express.json();
+app.use(express.json());
+
+
 const db = mysql.createConnection({
     host : 'localhost',
     user: process.env.USER,
@@ -16,7 +21,104 @@ db.connect((err) => {
     console.log('Connected to DB');
 })
 
-const app = express();
+app.get('/songs', (req,res) => {
+    let sql = `SELECT * FROM songs`;
+    let query = db.query(sql,(err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.get('/songs/:id', (req,res) => {
+    let sql = `SELECT * FROM songs WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    }); 
+});
+
+app.get('/artists/:id', (req,res) => {
+    let sql = `SELECT * FROM artists WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    }); 
+});
+
+app.get('/albums/:id', (req,res) => {
+    let sql = `SELECT * FROM albums WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    }); 
+});
+
+app.get('/playlists/:id', (req,res) => {
+    let sql = `SELECT * FROM playlists WHERE id = ${req.params.id}`;
+    let query = db.query(sql, (err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    }); 
+});
+
+app.post('/playlist' , (req,res) => {
+    let body = req.body;
+    let sql = `INSERT INTO playlists SET ?`
+    let newPlaylist = 
+    {
+        "name" : body.name,
+        "cover_img": body.cover_img,
+        "created_at": body.created_at 
+    };
+    db.query(sql,newPlaylist,(err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+});
+
+app.post('/album' , (req,res) => {
+    let body = req.body;
+    let sql = `INSERT INTO albums SET ?`
+    let newAlbum = 
+    {
+        "name" : body.name,
+        "artist_id": body.artist_id,
+        "cover_img": body.cover_img,
+        "created_at": body.created_at 
+    };
+    db.query(sql,newAlbum,(err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+});
+
+app.post('/artist' , (req,res) => {
+    let body = req.body;
+    let sql = `INSERT INTO artists SET ?`
+    let newArtist = 
+    {
+        "name" : body.name,
+        "cover_img": body.cover_img,
+        "upload_at": body.upload_at
+    };
+    db.query(sql,newArtist,(err,result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+});
+
+
+
+
+
 
 app.listen('3000', ()=> {
     console.log("Listening to port 3000...")
