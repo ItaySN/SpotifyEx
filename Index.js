d433116bb63aa69a149ac6c258edd9f96cd0ea37
ItaySN <itayfic@gmail.com>
2020-09-12 22:01:27 +0300

    Continue with the API requests of the app
    a POST request to /song add to songs
    
    a PUT request to /song/123 update the details of song 123
    
    a PUT request to /artist/123 update the artist 123
    
    a PUT request to /album/123 update the album 123
    
    a PUT request to /playlist/123 update the playlist 123

--- Index.js
+++ Index.js
@@ -115,7 +115,108 @@ app.post('/artist' , (req,res) => {
     })
 });
 
+app.post('/song', (req,res) =>{
+    let body = req.body;
+    let sql = `INSERT INTO songs SET ?`
+    let newSong = 
+    {
+        "title" : body.title,
+        "artist_id": body.artist_id,
+        "album_id": body.album_id,
+        "length":body.length ,
+        "track_number":body.track_number,
+        "lyrics": body.lyrics,
+        "created_at": body.created_at,
+        "youtube_link": body.youtube_link
+    };
+    db.query(sql,newSong,(err,result) => {
+        if(err)
+        {
+            if(err.errno === 1452){
+                res.status(404).send("an invalid artist or album id's has been submited");
+            }else{
+                return res.status(400).send(err.message);
+            }
+        }else{
+            return res.status(201).json(body);
+        }
+        console.log(result);
+    })
+});
 
+app.put('/songs/:id' , (req,res) =>{
+    let body = req.body;
+    let sql = `Update songs SET ?  WHERE id = ${req.params.id}`
+    let newSong = 
+    {
+        "title" : body.title,
+        "artist_id": body.artist_id,
+        "album_id": body.album_id,
+        "length":body.length ,
+        "track_number":body.track_number,
+        "lyrics": body.lyrics,
+        "created_at": body.created_at,
+        "youtube_link": body.youtube_link
+    };
+    db.query(sql,newSong,(err,result) => {
+        if(err)
+        {
+            if(err.errno === 1452){
+                return res.status(404).send("an invalid artist or album id's has been submited");
+            }else{
+                return res.status(400).send(err.message);
+            }
+        }else{
+            return res.status(204).json(body);
+        }
+    })
+})
+
+app.put('/artist/:id' , (req,res) => {
+    let body = req.body;
+    let sql = `Update artists SET ? WHERE id = ${req.params.id}`; 
+    let newArtist = 
+    {
+        "name" : body.name,
+        "cover_img": body.cover_img,
+        "upload_at": body.upload_at
+    };
+    db.query(sql,newArtist,(err,result) => {
+        if(err) throw err;
+        return res.status(204).json(body);
+    })
+});
+
+app.put('/albums/:id' , (req,res) => {
+    let body = req.body;
+    let sql = `Update albums SET ? WHERE id = ${req.params.id}`; 
+    let newAlbum = 
+    {
+        "name" : body.name,
+        "artist_id": body.artist_id,
+        "cover_img": body.cover_img,
+        "created_at": body.created_at
+    };
+    db.query(sql,newAlbum,(err,result) => {
+        if(err) throw err;
+        return res.status(204).json(body);
+    })
+});
+
+app.put('/playlist/:id' , (req,res) => {
+    let body = req.body;
+    let sql = `Update playlists SET ? WHERE id = ${req.params.id}`; 
+    let newPlaylist = 
+    {
+        "name" : body.name,
+        "cover_img": body.cover_img,
+        "created_at": body.created_at
+    };
+    db.query(sql,newArtist,(err,result) => {
+        if(err) throw err;
+        return res.status(204).json(body);
+    })
+});
 
 
 
