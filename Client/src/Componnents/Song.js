6902b63cac9a2803310ceb72ca6996097cd0be34
ItaySN <itayfic@gmail.com>
2020-09-15 04:23:31 +0300

    Fixing names and Css

--- Client/src/Componnents/Song.js
+++ Client/src/Componnents/Song.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import './Song.css'
+import { IconContext } from "react-icons";
+import { AiOutlineYoutube } from 'react-icons/ai';
+
+
 
 export default function Song({data}){
     
     return (
         <>
-        <div className="container">
+        <div className="containerSong">
             {data &&
-            <div className="grid-container">
                 <div className="song">
                     <p className="title">{data.title} </p>
-                    <p className="artist">{data.artist} </p>
+                    <p className="artistName">{data.artist} </p>
                     <p className="album">{data.album} </p>
-                    <a href={data.youtube_link} className="youtube_link">YouTube </a>
+                    <IconContext.Provider value={{size:"2.5em"}}>
+                    <a href={data.youtube_link} className="youtube_link" target="_blank"><AiOutlineYoutube/> </a>
+                    </IconContext.Provider>
                     {data.Num_Of_Listening && 
                     <p className="Num_Of_Listening">Num of listening : {data.Num_Of_Listening}</p>
                     }
                 </div>
-            </div>
             }
         </div>
         </>
