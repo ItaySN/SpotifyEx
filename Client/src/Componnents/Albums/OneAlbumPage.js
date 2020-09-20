cc2ea32f62c2d1de0b133e3869c5f29644969d6f
ItaySN <itayfic@gmail.com>
2020-09-20 08:04:32 +0300

    Create a one page for one album

---
+++ Client/src/Componnents/Albums/OneAlbumPage.js
@@ -0,0 +1,58 @@
+import React,{Component, useEffect,useState} from 'react'
+import axios from 'axios'
+import {useParams} from "react-router-dom";
+import Header from '../Header';
+import Song from '../Songs/Song';
+import Carousel from 'react-elastic-carousel';
+import {Link} from "react-router-dom";
+import './OneAlbumPage.css'
+import { Button } from 'react-bootstrap';
+
+function OneAlbumPage() {
+    const {id} = useParams();
+    const [album,setAlbum] = useState();
+    const [songs,setSongs] = useState([]);
+
+    useEffect(() => {  
+        (async () => {
+        try{
+            const {data} = await axios.get(`/albums/${id}`);
+            setAlbum(data[0])
+            const res = await axios.get(`/songs_ByAlbum/${id}`)
+            setSongs(res.data);
+            
+        } catch (err) {
+            console.error(err.message);
+        }
+    })();
+    },[id])
+
+    const breakPoints = [
+        { width: 1, itemsToShow: 1 },
+        { width: 450, itemsToShow: 2 },
+        { width: 700, itemsToShow: 3 },
+    ]
+
+    return (
+        album ?
+        <>
+        <Header/>
+        <div style={{color:"white",textAlign:"center",alignItems:"center"}}>
+            <div className="OneAlbumPageArtistImgDiv">
+                <Link style={{textDecoration:"none"}} to={`/album/${album.id}`}><img className="oneAlbumPageImgAlbum" src={album.cover_img}></img></Link> 
+            </div>
+                <h1><Link style={{textDecoration:"none"}} to={`/album/${album.id}`}>{album.name}</Link></h1>
+            <p className="oneAlbumPageNameArtist"><h2 style={{textAlign:"center"}}><Link style={{textDecoration:"none"}} to={`/artist/${album.artist_id}`}>{album.artist}<span className="OneAlbumArtistImgSpan"><img className="OneAlbumArtistImg" src={album.artist_img}></img></span></Link></h2></p>
+            <Carousel breakPoints={breakPoints}>
+                {songs.map(song => {
+                return <Song key={id} data={song} displayFromAllPage={false} />
+                })}
+            </Carousel>
+
+        </div>
+        </>
+        : <div></div>
+    )
+}
+
+export default OneAlbumPage;
--- Client/src/Componnents/Albums/TopAlbum.js
+++ Client/src/Componnents/Albums/TopAlbum.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './TopAlbum.css'
+import {Link} from "react-router-dom";
 
 function TopAlbum({data}) {
     return (
         <div className="oneTopAlbumDiv">
-            <img className="cover_TopAlbum" src={data.album_img}></img>
-            <h4>{data.album}</h4>
+            <Link style={{textDecoration:"none"}} to={`/album/${data.album_id}`}><img className="cover_TopAlbum" src={data.album_img}></img></Link>
+            <h4> <Link style={{textDecoration:"none"}} to={`/album/${data.album_id}`}>{data.album}</Link></h4>
         </div>
     )
 }
