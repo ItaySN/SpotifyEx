b29524c2d3b57554f829fa5bdf8e76ebc225a0d7
ItaySN <itayfic@gmail.com>
2020-09-20 08:08:14 +0300

    change the way that the data of the song display
    - the data style changes between the differnts componnets that use the song componnet.

--- Client/src/Componnents/Songs/Song.js
+++ Client/src/Componnents/Songs/Song.js
@@ -2,25 +2,49 @@ import React from 'react'
 import './Song.css'
 import { IconContext } from "react-icons";
 import { AiOutlineYoutube } from 'react-icons/ai';
+import {Link} from "react-router-dom";
 
 
 
 
-export default function Song({data}){
+
+export default function Song({data,displayFromAllPage=true,playlistSet=false}){
     
     return (
         <>
         <div className="containerOneSong">
             {data &&
-                <div className="oneSong">
-                    <p className="allSongsTitleP">{data.title} </p>
-                    <p className="allSongsArtistP"><img className="allSongsArtistImg" src={data.artist_img}></img><span className="allSongsArtistName">{data.artist}</span></p>
-                    <p className="allSongsAlbumP"><img className="allSongsAlbumImg" src={data.album_img}></img><span className="allSongsAlbumName">{data.album}</span></p>
-                    <IconContext.Provider value={{size:"2.5em"}}>
-                    <a href={data.youtube_link} className="youtube_link" target="_blank"><AiOutlineYoutube/> </a>
-                    </IconContext.Provider>
-                </div>
+                displayFromAllPage ? 
+                    <div className="oneSong">
+                        <p className="allSongsTitleP">{data.title}</p>
+                        <p className="allSongsArtistP"><Link style={{textDecoration:"none"}} to={`/artist/${data.artist_id}`}><img className="allSongsArtistImg" src={data.artist_img}></img><span className="allSongsArtistName">{data.artist}</span></Link></p>
+                        <p className="allSongsAlbumP"><Link style={{textDecoration:"none"}} to={`/album/${data.album_id}`}><img className="allSongsAlbumImg" src={data.album_img}></img><span className="allSongsAlbumName">{data.album}</span></Link></p>
+                        <IconContext.Provider value={{size:"2.5em"}}>
+                        <a href={data.youtube_link} className="youtube_link" target="_blank"><AiOutlineYoutube/> </a>
+                        </IconContext.Provider>
+                    </div>
+                :  
+                    <div>
+                        {playlistSet ? 
+                        <p><Link style={{textDecoration:"none"}} to={`/album/${data.album_id}`}><img className="squaringImgAlbum" src={data.album_img}></img></Link></p>
+                        : <IconContext.Provider value={{size:"4.5em"}}>
+                        <Link style={{textDecoration:"none"}}>
+                        <AiOutlineYoutube/> 
+                        </Link>
+                        </IconContext.Provider>
+                    }
+                        <h4>{data.title}</h4>
+                        {playlistSet ? 
+                            <p><Link style={{textDecoration:"none"}} to={`/artist/${data.artist_id}`}>{data.artist}</Link></p>
+                            : <></>
+                        }
+                        <p>{data.length}</p>
+                        
+
+                    </div>
+               
             }
+            
         </div>
         </>
     )
--- Client/src/Componnents/Songs/TopSong.js
+++ Client/src/Componnents/Songs/TopSong.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import './TopSong.css';
 import {Link} from "react-router-dom";
+import { IconContext } from "react-icons";
+import { AiOutlineYoutube } from 'react-icons/ai';
 
 
-function TopSong({data}) {
+function TopSong({data,displayFromHomePage=true}) {
     return (
+        
         <div className="oneTopSongDiv">
-            <img className="cover_imgAlbum" src={data.album_img}></img>
-            <h4>{data.title}</h4>
-            <p className="artistAndLength"><span>{data.artist}</span>|<span>{data.length}</span></p>
+            {
+                displayFromHomePage ?
+                   <img className="cover_imgAlbum" src={data.album_img}></img>
+                : <div className="playImgOfSongs"> <IconContext.Provider value={{size:"5em"}}> 
+                    <Link style={{textDecoration:"none"}}><AiOutlineYoutube/></Link>
+                </IconContext.Provider> </div>
+                        
+                        
+                        
+            }
+            { displayFromHomePage ?
+                <h4>{data.title}</h4>
+                : <h4>{data.song_name}</h4>
+            }
+            {
+                displayFromHomePage ?
+                <p className="artistAndLength"><span><Link style={{textDecoration:"none",color:"black"}} to={`/artist/${data.artist_id}`}>{data.artist}</Link></span>|<span>{data.length}</span></p>
+                :  <p className="artistAndLength"><span>{data.length}</span></p>
+            }
         </div>
+        
     )
 }
 
