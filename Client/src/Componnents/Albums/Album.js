84586c89c0a7e5f9f3a7183927dc2e7132f5cfe1
ItaySN <itayfic@gmail.com>
2020-09-20 08:03:50 +0300

    makes two styles for the albums componnent
    The information depands about who want to show the albums.

--- Client/src/Componnents/Albums/Album.js
+++ Client/src/Componnents/Albums/Album.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import './Album.css';
+import { Link } from 'react-router-dom'
 
-function Album({data}) {
+function Album({data,displayName=true}) {
     return (
         <>
         <div className="allAlbumsRow">
             <div className="containerOneAlbum">
                 {data &&
-                    <div className="oneAlbum">
-                        <div className="allAlbumsImgDiv"><img className="allAlbumsCoverImg" src={data.cover_img}></img></div>
-                        <h4 className="allAlbumsNameP">{data.name}</h4>
-                        <p key={data.artist_id} className="allAbumsArtistP">{data.artist}</p>
-                    </div>
+                    displayName ?
+                        <div className="oneAlbum">
+                            <div className="allAlbumsImgDiv"><Link style={{textDecoration:"none"}} to={`/album/${data.id}`}><img className="allAlbumsCoverImg" src={data.cover_img}></img></Link></div>
+                            <h4 className="allAlbumsNameP"><Link style={{textDecoration:"none"}} to={`/album/${data.id}`}>{data.name}</Link></h4>
+                            {displayName&&
+                            <p key={data.artist_id} className="allAbumsArtistP"><Link style={{textDecoration:"none"}} to={`/artist/${data.artist_id}`}>{data.artist}</Link></p>
+                            }
+                        </div>
+                    : <div className="oneAlbumNoAllAlbums">
+                            <div className="NoAllAlbumsImgDiv"><Link style={{textDecoration:"none"}} to={`/album/${data.id}`}><img className="NoallAlbumsCoverImg" src={data.cover_img}></img></Link></div>
+                            <h4 className="NoAllAlbumsNameP"><Link style={{textDecoration:"none"}} to={`/album/${data.id}`}>{data.name}</Link></h4>
+                        </div>
                 }
             </div>
         </div>
